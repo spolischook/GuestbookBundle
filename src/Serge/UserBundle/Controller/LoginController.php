5e2d451f748d8f495f39993a4ba000191369e921
SPolischook <spolischook@gmail.com>
2012-11-21 10:28:06 +0200

    Add UserBundle

---
+++ src/Serge/UserBundle/Controller/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Serge\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UserController extends Controller
+{
+    public function loginAction()
+    {
+
+    }
+}
