3c67d9a0818d6c9396965df359c38dfb10aa8144
SPolischook <spolischook@gmail.com>
2012-11-21 11:11:38 +0200

    Do annotation routing

--- src/Serge/UserBundle/Controller/LoginController.php
+++ src/Serge/UserBundle/Controller/LoginController.php
@@ -3,9 +3,13 @@
 namespace Serge\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class LoginController extends Controller
 {
+    /**
+     * @Route("/login", name="login")
+     */
     public function loginAction()
     {
 
